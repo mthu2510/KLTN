5180a3b41611a84a81df60e75d02c4b4d9a981c6
mthu2510 <nguyendamminhthu@gmail.com>
2025-03-22 02:45:38 +0700

    second update

--- ex1.py
+++ ex1.py
@@ -13,48 +13,58 @@ E_vals = np.logspace(-1, 2, 100)  # Energy values from 0.1 GeV to 100 GeV
 num_zeros = 100  # Number of first zeros of J0 to use
 
 # Find the first zeros of J0(x)
-zeros_j0 = sp.special.jn_zeros(0, num_zeros)
+zeros_j0 = sp.special.jn_zeros(0, num_zeros) # zeta_n
 
 def func_gSNR(r):
     return 1.0 / (np.pi * R**2) # kpc^-2
 
 r = np.linspace(0, R, 10000)
-gr = func_gSNR(r)*r**0
+gr = func_gSNR(r) * r**0
 dr = np.append(np.diff(r),0)
 
-j0_n = sp.special.j0(zeros_j0[:,np.newaxis] * r[np.newaxis,:]/R)
+j0_n = sp.special.j0(zeros_j0[:,np.newaxis] * r[np.newaxis,:] / R)
 coeff_gSNR = np.sum(r[np.newaxis,:] * gr[np.newaxis,:] * j0_n * dr[np.newaxis,:], axis=1)
 coeff_gSNR *= (2.0 / (R * sp.special.j1(zeros_j0))**2)
 
 gr_test = np.sum(j0_n * coeff_gSNR[:,np.newaxis], axis=0)
 
-print(gr[0:10])
-print(gr_test[0:10])
+"""print(gr[0:10])
+print(gr_test[0:10])"""
 
-"""def compute_j_E(E_vals, num_terms=10):
+def Q_E_func(E):
+    return (E / 1.0) ** -2.2
+
+Q_E = Q_E_func(E_vals)
+
+def compute_j_E(E_vals, num_zeros):
     # Compute j(E) based on the zeros of the Bessel function of order 0
     D_E = D0 * (E_vals / 1.0) ** (1/3)  # D(E) according to the problem
     j_E = np.zeros_like(E_vals)
 
-    for i, E in enumerate(E_vals):
-        sum_term = 0
-        for n in range(num_terms):
-            zeta_n = zeros_J0[n]
-            C_n = (zeta_n ** 2) * D_E[i] / R**2
-            S_n = np.sqrt(u0**2 / (D_E[i]**2) + 4 * zeta_n**2 / R**2)
+    g_SNR_coeff = coeff_gSNR
 
-            A_n = - (g_SNR * D_E[i]) / (2 * np.exp(S_n * H / 2) * np.sinh(S_n * H / 2) * (u0 + D_E[i] * S_n / np.tanh(S_n * H / 2)))
-            f_nz_0 = -2 * A_n * np.exp(u0 * z / (2 * D_E[i]) + S_n * H / 2) * np.sinh(S_n * (H - z) / 2)
+    for i, E in enumerate(E_vals):
+        f_z = 0
+        for n in range(num_zeros):
+            zeta_n = zeros_j0[n]
+
+            S_n = np.sqrt(u0**2 / (D_E[i]**2) + 4 * zeta_n**2 / R**2)
+            coth_SnH = 1 / np.tanh(S_n * H / 2)
+
+            J0_rval = sp.special.j0(zeta_n * r_val / R)  # Compute J0 at r_val
+            g_SNR_n = g_SNR_coeff[n]  # Get coefficient for mode n
+
+            A_n = - (Q_E[i] * g_SNR_n) / (2 * np.exp(S_n * H / 2) * np.sinh(S_n * H / 2) * (u0 + D_E[i] * S_n * coth_SnH))
             
-            sum_term += np.abs(A_n) * np.exp(-C_n * z) * np.sin(zeta_n * r_val / R) * f_nz_0
+            f_z += A_n * J0_rval * np.exp(u0 * z / (2 * D_E[i]) + S_n * H / 2) * np.sinh(S_n * (H - z) / 2) / np.sinh(S_n * H / 2)
 
         v = np.sqrt(2 * E * 1.6e-6 / 9.11e-28)  # Compute particle velocity (cm/s) from energy (1 GeV = 1.6e-6 erg)
-        j_E[i] = v * sum_term / (4 * np.pi)
+        j_E[i] = v * f_z / (4 * np.pi)
     
     return j_E
 
 # Compute j(E)
-j_E_vals = compute_j_E(E_vals)
+j_E_vals = compute_j_E(E_vals, num_zeros)
 
 # Plot the graph
 plt.figure(figsize=(8, 6))
@@ -64,4 +74,4 @@ plt.ylabel('j(E)')
 plt.title('Particle Spectrum j(E)')
 plt.legend()
 plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-plt.show()"""
+plt.show()
